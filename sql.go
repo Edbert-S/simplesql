6da275c2b6d112ce58de5531af6c871d75f2d1a7
Edbert-S <edbert.suwandy@everquote.com>
2023-10-12 16:35:26 +0100

    init

---
+++ sql.go
@@ -0,0 +1,49 @@
+package simpleSQL
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DB interface {
+	//methods
+	Database()
+	Query() sql.Rows
+	CloseDB() error
+}
+
+type Database struct {
+	name string
+	db   *sql.DB
+}
+
+func (d Database) new(name string, db *sql.DB) {
+	d.name = name
+	d.db = db
+}
+
+func (d Database) Connect() (*sql.DB, error) {
+	return nil, fmt.Errorf("Can't use Abstract functions directly")
+}
+
+func (d *Database) Query(query string) (rows *sql.Rows, err error) {
+	prepedQuery, err := d.db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := prepedQuery.Query()
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func (d *Database) CloseDB() error {
+	err := d.db.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ sqlSnowflake.go
@@ -0,0 +1,62 @@
+package simpleSQL
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/snowflakedb/gosnowflake"
+)
+
+type snowflake struct {
+	Database
+	SnowflakeAuth
+}
+
+type SnowflakeAuth struct {
+	authType  string
+	username  string
+	password  string
+	org       string
+	connected bool
+}
+
+func (s snowflake) new(db_name string, auth SnowflakeAuth) error {
+	s.SnowflakeAuth = auth
+	db, err := s.connect()
+	if err != nil {
+		return err
+	}
+	s.Database.new(db_name, db)
+	return nil
+}
+
+func (s snowflake) connect() (*sql.DB, error) {
+	auth := s.SnowflakeAuth
+	if auth.authType == "okta" {
+		db, err := s.okta_connect()
+		return db, err
+	} else {
+		return nil, fmt.Errorf("Invalid authType")
+	}
+}
+
+func (s snowflake) okta_connect() (*sql.DB, error) {
+	a := s.SnowflakeAuth
+	url := fmt.Sprintf("https://%v.okta.com@%v-%v/%v", a.username, a.org, a.org, s.name)
+	db, err := sql.Open("snowflake", url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (s snowflake) CloseDB() error {
+	err := s.CloseDB()
+	return err
+}
+
+func (s snowflake) Query(query string) (sql.Rows, error) {
+	return s.Query(query)
+}
